afd680a49b864a98e908cff3359e683c4706672e
Thai Vo <dythirk@gmail.com>
2024-08-07 14:26:26 +0000

    Final CombineArrays

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -9,16 +9,16 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
 
+ //   ArrayList<Integer> combined = new ArrayList<Integer>();
     ArrayList<Integer> combined = new ArrayList<Integer>();
-
     public int[] combine(int[] arr1, int[] arr2){
 
         for (int i = 0; i < arr1.length; i++){
-            combined.add(arr1[i])
+            combined.add(arr1[i]);
         }
       
         for (int i = 0; i < arr2.length; i++){
-            combined.add(arr2[i])
+            combined.add(arr2[i]);
         }
 
         return combined;
